7d2d8036decd132efe36b7aa0c42ca9d27789dd4
Vinisha Yadav <vinishadv@gmail.com>
2025-12-23 21:31:20 +0530

    fix(ui):resolved error

--- src/routes/app/+layout.svelte
+++ src/routes/app/+layout.svelte
@@ -1,24 +1,27 @@
 <script lang="ts">
+  import { page } from '$app/stores';
   import { gql } from '$lib/client/gql';
   export let data: { user: { email: string } };
 
   const LOGOUT = `mutation { logout }`;
 
-  function pathActive(path: string) {
-    return location?.pathname?.startsWith(path) ? 'active' : '';
+  function isActive($page: any, path: string) {
+    return $page.url.pathname.startsWith(path) ? 'active' : '';
   }
 
   async function logout() {
     await gql<{ logout: boolean }>(LOGOUT);
-    location.href = '/auth/login';
+    window.location.href = '/auth/login';
   }
 </script>
 
+
 <div class="nav">
   <div class="nav-inner">
-    <a class={pathActive('/app/tasks')} href="/app/tasks">Tasks</a>
-    <a class={pathActive('/app/summary')} href="/app/summary">Today</a>
-    <a class={pathActive('/app/logs')} href="/app/logs">Logs</a>
+<a class={$page && isActive($page, '/app/tasks')} href="/app/tasks">Tasks</a>
+<a class={$page && isActive($page, '/app/summary')} href="/app/summary">Today</a>
+<a class={$page && isActive($page, '/app/logs')} href="/app/logs">Logs</a>
+
 
     <div style="margin-left:auto" class="row">
       <span class="badge">{data.user.email}</span>
